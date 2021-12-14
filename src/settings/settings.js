e75e8233c380e4ed13a9c23f213558373d0f5bfa
MaciejP <m.ponicki1995@gmail.com>
2021-12-14 21:53:32 +0100

    feat: add changes from code review add background in styles.scss add dist folder to .gitignore

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,11 +1,22 @@
 import "./settings.scss";
-import { setDataInStorage } from "../utils/localStorage/localStorage.js";
+import bodyStyles from "../styles.scss";
+import {
+  setDataInStorage,
+  getDataFromStorage,
+} from "../utils/localStorage/localStorage.js";
 
-const SAVE_BUTTON = document.querySelector(".section__saveButton");
+const SAVE_BUTTON = document.querySelector(".section__button--save");
 const INPUT = document.querySelector(".section__input--playerName");
 
-const clickToSavePlayerName = () => {
+const onbuttonClick = () => {
   setDataInStorage("playerName", INPUT.value);
 };
 
-SAVE_BUTTON.addEventListener("click", clickToSavePlayerName);
+function getPlayerNameIfExists() {
+  if (localStorage.length > 0) {
+    INPUT.value = getDataFromStorage("playerName");
+  }
+}
+
+getPlayerNameIfExists();
+SAVE_BUTTON.addEventListener("click", onbuttonClick);
