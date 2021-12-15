8919539b83ca4e5ba0bf287f76e8a75a5e272e99
MaciekPon <m.ponicki@interia.pl>
2021-12-15 17:37:08 +0100

    feat(settings): change functions name

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -8,11 +8,11 @@ import {
 const SAVE_BUTTON = document.querySelector(".section__button--save");
 const INPUT = document.querySelector(".section__input--playerName");
 
-const onbuttonClick = () => {
+const onButtonClick = () => {
   setDataInStorage("playerName", INPUT.value);
 };
 
-function getPlayerNameIfExists() {
+function setPlayerName() {
   
   const playerName = getDataFromStorage("playerName");
 
@@ -21,5 +21,5 @@ function getPlayerNameIfExists() {
   }
 }
 
-getPlayerNameIfExists();
-SAVE_BUTTON.addEventListener("click", onbuttonClick);
+setPlayerName();
+SAVE_BUTTON.addEventListener("click", onButtonClick);
