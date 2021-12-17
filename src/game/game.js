84ed0aeb7566cb89c79667766439c4caa22848e6
JustD1d1t <starzyk.dawid.0826@gmail.com>
2021-12-17 08:37:44 +0100

    refactor: remove game.js exmaple start game

--- src/game/game.js
+++ src/game/game.js
@@ -3,25 +3,3 @@ import { getResult } from "./gameResult";
 import { displayModal } from "./gameOverModal";
 import "./gameOverModal.scss";
 import "../styles.scss";
-
-import {
-  setDataInStorage,
-  getDataFromStorage,
-} from "../utils/localStorage/localStorage";
-
-setDataInStorage("computerPoints", 5);
-setDataInStorage("userPoints", 8);
-
-let time = setTimer(2, () => {
-  stopGame();
-});
-
-function stopGame() {
-  const { settlement, result, userPoints } = getResult();
-  stopTimer();
-  displayModal(settlement, result, userPoints);
-}
-
-setTimeout(() => {
-  stopGame();
-}, 3000);
