357df228721c3a4203fbee5d0af945157bb4105a
Marcin Rycko <66424603+MarcinRycko@users.noreply.github.com>
2021-12-18 19:03:33 +0100

    Feature/gameboard (#5)
    * Initial commit
    
    * fix: implement game files into an existing file structure
    
    * fix: export SHIPS_LIST to constants.js, add dist to gitignore, change h1 to p, change classes names
    
    * fix: change classes names, use SCREAMING_SNAKE_CASE for const variables
    
    * fix: delete whitespace, change function name

--- src/game/game.js
+++ src/game/game.js
@@ -1,5 +1,18 @@
 import { setTimer, stopTimer } from "./gameTimer";
 import { getResult } from "./gameResult";
 import { displayModal } from "./gameOverModal";
-import "./gameOverModal.scss";
+import { createGameBoards, checkIfGameStarted } from "./createGameBoards";
 import "../styles.scss";
+import "./game.scss";
+import "./gameOverModal.scss";
+
+const START_GAME_BTN = document.querySelector(".button--start");
+let isGameStarted = false;
+
+START_GAME_BTN.addEventListener("click", function () {
+  isGameStarted = !isGameStarted;
+  checkIfGameStarted(isGameStarted, START_GAME_BTN);
+});
+
+createGameBoards();
+checkIfGameStarted(isGameStarted);
