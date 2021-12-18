e7fde854148032b99a73e4716d142345fb397890
JustD1d1t <starzyk.dawid.0826@gmail.com>
2021-12-18 11:31:01 +0100

    tests: add more scenarios and separate success and fail tests

--- src/game/test/gameModal.test.js
+++ src/game/test/gameModal.test.js
@@ -9,10 +9,27 @@ describe("Use modal function", () => {
     test.each`
       input
       ${{ settlement: "Victory", result: "win", points: 8 }}
+      ${{ settlement: "Defeat", result: "lose", points: 3 }}
     `("and check is modal inside dom", ({ input, expected }) => {
       displayModal(input.settlement, input.result, input.points);
       const modal = document.querySelector(".modal");
       expect(modal).toBeInTheDocument;
+    });
+  });
+  describe("called displayModal with wrong provided parameters", () => {
+    test.each`
+      input
+      ${{ settlement: true, result: "win", points: 8 }}
+      ${{ settlement: "Victory", result: null, points: 8 }}
+      ${{ settlement: "Victory", result: "win", points: "test" }}
+      ${{ settlement: null, result: "win", points: undefined }}
+      ${{ settlement: "Victory", result: "lose", points: undefined }}
+      ${{ settlement: "Defeat", result: "win", points: undefined }}
+    `("and catch error", ({ input, expected }) => {
+      try {
+        displayModal(input.settlement, input.result, input.points);
+      } catch (err) {}
+      expect(displayModal).toThrowError;
     });
   });
   describe("called displayModal with provided parameters", () => {
