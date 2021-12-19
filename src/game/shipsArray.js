91b4a8ae1f0a92dacb1ce8136f8c9cdde6d9cef3
JustD1d1t <starzyk.dawid.0826@gmail.com>
2021-12-19 12:11:41 +0100

    feat: add ships array, delete unnecessary tests

---
+++ src/game/shipsArray.js
@@ -0,0 +1,190 @@
+let PLAYER_SHIPS = {
+  ships: [
+    {
+      name: "Carrier",
+      fields: [
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+      ],
+      sunk: false,
+    },
+    {
+      name: "Battleship",
+      fields: [
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+      ],
+      sunk: false,
+    },
+    {
+      name: "Destroyer",
+      fields: [
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+      ],
+      sunk: false,
+    },
+    {
+      name: "Submarine",
+      fields: [
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+      ],
+      sunk: false,
+    },
+    {
+      name: "Patrol Boat",
+      fields: [
+        {
+          name: null,
+          hit: false,
+        },
+      ],
+      sunk: false,
+    },
+  ],
+  defeat: false,
+};
+let COMPUTER_SHIPS = {
+  ships: [
+    {
+      name: "Carrier",
+      fields: [
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+      ],
+      sunk: false,
+    },
+    {
+      name: "Battleship",
+      fields: [
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+      ],
+      sunk: false,
+    },
+    {
+      name: "Destroyer",
+      fields: [
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+      ],
+      sunk: false,
+    },
+    {
+      name: "Submarine",
+      fields: [
+        {
+          name: null,
+          hit: false,
+        },
+        {
+          name: null,
+          hit: false,
+        },
+      ],
+      sunk: false,
+    },
+    {
+      name: "Patrol Boat",
+      fields: [
+        {
+          name: null,
+          hit: false,
+        },
+      ],
+      sunk: false,
+    },
+  ],
+  defeat: false,
+};
