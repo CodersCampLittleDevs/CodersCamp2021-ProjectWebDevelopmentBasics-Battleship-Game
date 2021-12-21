6bb2ea8fe52d4db4bb0ab458b08eba091617d06f
MaciejP <m.ponicki1995@gmail.com>
2021-12-21 21:59:16 +0100

    feat(settings): add tests to functions in settings.js add hover for return link add hover for save button change exports for localStorage functions

---
+++ src/settings/settings.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+const button = document.createElement("button");
+button.classList.add("section__button--save");
+document.body.append(button);
+
+const input = document.createElement("input");
+input.classList.add("section__input--playerName");
+document.body.append(input);
+
+const { setPlayerName, onButtonClick } = require("./settings");
+
+describe("Using function to set and get data from local storage", () => {
+  it("should set player's name on button click", () => {
+    input.value = "test-name";
+    onButtonClick();
+    expect(localStorage.getItem("playerName")).toBe("test-name");
+  });
+
+  it("should get player's name into input from storage", () => {
+    localStorage.setItem("playerName", "test-name");
+    setPlayerName();
+    expect(input.value).toBe("test-name");
+  });
+});
