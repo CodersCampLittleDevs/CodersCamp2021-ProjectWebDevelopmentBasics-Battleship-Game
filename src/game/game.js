1943a2c26cd638b7ad94db679d00cf62954189ac
Marcin Rycko <66424603+MarcinRycko@users.noreply.github.com>
2021-12-23 21:45:10 +0100

    Feature/bg 11 (#13)
    * feat: Initial commit
    
    * feat: show shpis on board, change board array to array of objects
    
    * fix: remove number object, code optimization
    
    * fix: refactor code
    
    * fix: refactor code
    
    * fix: refactor code

--- src/game/game.js
+++ src/game/game.js
@@ -2,12 +2,22 @@ import { setTimer, stopTimer } from "./gameTimer";
 import { getResult } from "./gameResult";
 import { displayModal } from "./gameOverModal";
 import { createGameBoards, checkIfGameStarted } from "./createGameBoards";
+import { addShipsToBoard } from "./randomizationOfShipsPositions";
 import { generateShipsArray } from "./shipsArray";
 import { startGame, isGameStarted, START_GAME_BTN } from "./startGame";
 import "../styles.scss";
 import "./game.scss";
 import "./gameOverModal.scss";
 
+const RANDOMIZE_BTN = document.querySelector(".actions__button-randomize");
+
 createGameBoards();
 checkIfGameStarted(isGameStarted, START_GAME_BTN);
 START_GAME_BTN.addEventListener("click", startGame);
+
+RANDOMIZE_BTN.addEventListener("click", function () {
+  addShipsToBoard(true);
+});
+
+// only for test
+addShipsToBoard(false);
