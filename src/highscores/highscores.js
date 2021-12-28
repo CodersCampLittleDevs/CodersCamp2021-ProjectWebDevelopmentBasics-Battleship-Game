9b5c707302b11c3877358c0ea8fe227ed0dc365c
Alicja Guzik <95084583+alicjaguzik@users.noreply.github.com>
2021-12-28 11:24:06 +0100

    update highscores html, scss & js

--- src/highscores/highscores.js
+++ src/highscores/highscores.js
@@ -1,56 +1,44 @@
-import globalStyles from "../styles.scss";
-import styles from "./highscores.scss";
+import "../styles.scss";
+import "./highscores.scss";
 import {
-  setDataInStorage, 
-  getDataFromStorage
-} from '../utils/localStorage/localStorage';
+  setDataInStorage,
+  getDataFromStorage,
+} from "../utils/localStorage/localStorage";
 import { displayModal } from "../game/gameOverModal";
 
 export const saveMatchResult = (matchResult, points) => {
-  const playerName = getDataFromStorage('playerName') ?? 'Player';
-  const row = {name: playerName, score: points, status: matchResult};
-  const highScore = getDataFromStorage('highScore');
-    if (highScore) {
-      const highScores = [...JSON.parse(highScore), row].sort((a, b) => b.score - a.score).slice(0, 10);
-      setDataInStorage('highScore', JSON.stringify(highScores));
-    } else {
-      const newHighScores = [row];
-      setDataInStorage('highScore', JSON.stringify(newHighScores));
-    }
-}
+  const PLAYER_NAME = getDataFromStorage("playerName") ?? "Player";
+  const row = {
+    name: PLAYER_NAME,
+    score: points ?? "",
+    status: matchResult ?? "",
+  };
+  const HIGH_SCORES = getDataFromStorage("highScores");
+  if (HIGH_SCORES) {
+    const highScores = [...JSON.parse(HIGH_SCORES), row]
+      .sort((a, b) => b.score - a.score)
+      .slice(0, 10);
+    setDataInStorage("highScores", JSON.stringify(highScores));
+  } else {
+    const newHighScores = [row];
+    setDataInStorage("highScores", JSON.stringify(newHighScores));
+  }
+};
 
-const displayHighScore = () => {
-  const highScores = JSON.parse(getDataFromStorage('highScore'));
-  const names = [...document.querySelectorAll('.table__data-name')];
-  const scores = [...document.querySelectorAll('.table__data-score')];
-  const statuses = [...document.querySelectorAll('.table__data-status')];
+saveMatchResult();
 
-  names.forEach((name, index) => {
-  console.log( highScores)
-  name.innerHTML = highScores[index].name
-  });
+const displayHighScores = () => {
+  const HIGH_SCORES = JSON.parse(getDataFromStorage("highScores"));
+  for (let i = 0; i < 10; i++) {
+    const tr = document.createElement("tr");
+    tr.classList.add("table__row");
+    tr.innerHTML = `
+    <td class="row__cell">${i + 1}</td>
+    <td class="row__cell row__cell-name">${HIGH_SCORES[i].name}</td>
+    <td class="row__cell row__cell-score">${HIGH_SCORES[i].score}</td>
+    <td class="row__cell row__cell-status">${HIGH_SCORES[i].status}</td>`;
+    document.querySelector(".table").appendChild(tr);
+  }
+};
 
-  scores.forEach((score, index) => {
-  console.log( highScores)
-   score.innerHTML = highScores[index].score
-  });
-
-  statuses.forEach((stat, index) => {
-  console.log( highScores)
-   stat.innerHTML = highScores[index].status
-  });
-}
-
-displayHighScore();
-
-
-
-
-
-
-
-
-
-  
- 
-
+displayHighScores();
